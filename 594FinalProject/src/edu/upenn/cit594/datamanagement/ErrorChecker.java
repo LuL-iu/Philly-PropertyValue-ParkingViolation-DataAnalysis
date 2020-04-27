b91f983ddcb5ac92dea2711101af659a35c3a49e
lu <lu10@seas.upenn.edu>
2020-04-26 23:21:21 -0700

    commend main method in error checker

--- 594FinalProject/src/edu/upenn/cit594/datamanagement/ErrorChecker.java
+++ 594FinalProject/src/edu/upenn/cit594/datamanagement/ErrorChecker.java
@@ -76,7 +76,7 @@ public class ErrorChecker {
 		return false;
 	}
 
-//	public static void main(String[] args) {
+////public static void main(String[] args) {
 //	// TODO Auto-generated method stub
 //	String fileName = "population.txt";
 //	ErrorChecker eCheck = new ErrorChecker();
